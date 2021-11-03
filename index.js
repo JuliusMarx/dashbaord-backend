6cf53aacfad982d65a206b3a82e15214fb597b90
jmarx <j.marx@tarent.de>
2021-11-03 13:49:10 +0100

    Change port due to problems

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.delete('/api/users/:id', (req, res) => {
 });
 
 //PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 app.listen(port, () => console.log(`Listening on port ${port}...`)
 )
 
