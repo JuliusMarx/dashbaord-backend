50fba29fa16257ecbec6450d9d30906adc635979
jmarx <j.marx@tarent.de>
2021-11-03 13:43:05 +0100

    Add res header for cors origin

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const users = [
     { id: 3, name: 'user2' }
 ];
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome, please navigate to /api/users to read userdata.')
 })
